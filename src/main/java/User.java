c53cdde0f0244cdcebb7f8fd8a6261afd6b642f8
jyl <111@qq.com>
2020-03-08 10:00:54 +0800

    1

---
+++ src/main/java/User.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author
+ * @Date 2020/3/8 9:52
+ * @Version 1.0
+ **/
+public class User {
+    private String username;
+    private Date birthday;
+    private String sex;
+    private String address;
+    private String colour;
+}
