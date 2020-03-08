86336b5cc8155fd22bc6fe79c3d3752cf208a94a
jyl <111@qq.com>
2020-03-08 10:09:38 +0800

    3

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -8,10 +8,58 @@ import java.util.Date;
  * @Version 1.0
  **/
 public class User {
-    private String id;
+    private String Id;
     private String username;
     private Date birthday;
     private String sex;
     private String address;
     private String colour;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        this.Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
